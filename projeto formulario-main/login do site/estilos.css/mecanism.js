431cc89b2fd4b8fe94e79393dcbc29c4759b2e7a
euRenatoReis <115581375+euRenatoReis@users.noreply.github.com>
2022-11-29 17:14:12 -0300

    Add files via upload

---
+++ projeto formulario-main/login do site/estilos.css/mecanism.js
@@ -0,0 +1,31 @@
+
+//verificar se as senhas coencidem //
+  //chamar o input //
+const senha = document.getElementsByClassName("Senha1");
+const senha2 = document.getElementsByClassName("Senha1");
+  //verificar se um input é igual ao outro //
+
+ if(senha !== senha2)
+   {
+       alert("As senhas não coincidem")
+   }
+  //aplicar a condição se for diferente //
+
+//media queries//
+
+//verificar a idade minima //
+
+//campo vermelho caso algo não for certo //
+
+// aparecer pop-ups de senha (a senha deve conter:)//
+
+//icone de ocultar/visualizar senha //
+
+ const ocult = document.getElementsByClassName("ocultar");
+
+ if(senha.classlist.contains(type="password"))
+  {
+
+      senha.classlist.replace("password","name");
+
+  }
